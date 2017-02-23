33e5cd2c12e0e72067f9b4b7fbc70ce24f790a96
Ethan Bennett <ebennett19@gmail.com>
2017-02-23 11:42:49 -0700

    Refactoring as much as I can. All functionality and tests in place

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -2,5 +2,6 @@ class BestBuyService
   def self.store_info(zip)
     response_body = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=city,storeType,distance,longName,phone&pageSize=10&apiKey=#{ENV['KEY']}").body
     raw_info = JSON.parse(response_body)
+    raw_info
   end
 end
