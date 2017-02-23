89c010f5259f29a60662b2129791a35085af1da3
Ethan Bennett <ebennett19@gmail.com>
2017-02-23 10:01:38 -0700

    Increase service test coverage

--- spec/requests/best_buy_service_spec.rb
+++ spec/requests/best_buy_service_spec.rb
@@ -3,11 +3,19 @@ require 'rails_helper'
 RSpec.describe "Best Buy Service" do
   before do
     raw_info = BestBuyService.store_info("80202")
-    # binding.pry
     @stores = raw_info["stores"]
+    @store = @stores.first
   end
 
-  it "returns the expected number of responses" do
+  it "returns the expected number of results" do
     expect(@stores.count).to eq(10)
+  end
+
+  it "returns all desired paramaters for each result" do
+    expect(@store).to have_key("longName")
+    expect(@store).to have_key("distance")
+    expect(@store).to have_key("phone")
+    expect(@store).to have_key("city")
+    expect(@store).to have_key("storeType")
   end
 end
