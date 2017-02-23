b9d908eeb294025283d3958ea87ff7ecd534968e
Ethan Bennett <ebennett19@gmail.com>
2017-02-23 10:31:22 -0700

    Start TDDing API functionality

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :item do
+    name "Item"
+    description "Description"
+    image_url "google.com"
+  end
+end
--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Items API" do
+  before do
+    @items = create_list(:item, 3)
+    get api_v1_items_path
+  end
+
+  it "shows all expected results in index" do
+    expect(status).to eq(200)
+  end
 
 end
